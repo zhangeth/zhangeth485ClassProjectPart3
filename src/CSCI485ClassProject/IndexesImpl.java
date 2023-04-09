43167dc373073b47a347fdcda9ab56192d2581c0
Ethan Zhang <zhangeth@usc.edu>
2023-04-09 01:52:08 -0700

    added

--- src/CSCI485ClassProject/IndexesImpl.java
+++ src/CSCI485ClassProject/IndexesImpl.java
@@ -3,6 +3,11 @@ package CSCI485ClassProject;
 import CSCI485ClassProject.models.IndexType;
 
 public class IndexesImpl implements Indexes{
+
+  // default constructor for passing test case
+  public IndexesImpl()
+  {}
+
   @Override
   public StatusCode createIndex(String tableName, String attrName, IndexType indexType) {
     // your code
